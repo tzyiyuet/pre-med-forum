def03bf7d5447141f4378eff03e9b9f712c1f40f
Yiyuet <46637296+tzyiyuet@users.noreply.github.com>
2020-10-01 16:58:12 -0400

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,312 @@
+/*
+Deliverable 3: INSY 437
+Names: Alexa Hernandez (260743067), Sasha Kahzdan (260666176), Vittoria Vecchioli (260720859), Yiyue Tan (260825127) 
+*/
+
+# Create Member relation 
+
+CREATE TABLE Member ( 
+	username VARCHAR(20) NOT NULL,
+	date_joined DATE NOT NULL,
+	last_visited DATE NOT NULL,
+	password VARCHAR(20) NOT NULL,
+	gender VARCHAR(12) DEFAULT 'Not Telling', 
+	member_rank ENUM('Newbie', 'Senior Member', 'Moderator') DEFAULT 'Newbie', 
+	active BIT DEFAULT B'0',
+	about_me text,
+	PRIMARY KEY (username)
+); 
+
+# insert records into Member relation
+
+INSERT INTO Member VALUES
+        ('bazinga', '2017-01-01', '2019-03-15', 'bazingarules', 'male', 'Moderator', b'1', null),
+         ('premed_67', '2017-01-31', '2019-03-10', 'hello123', 'female', 'Senior Member', b'0', null),
+        ('doctorwho', '2018-04-04', '2019-03-16', 'froyo', 'male', 'Newbie', b'0', 'first year premed student'),
+         ('future_doc', '2019-01-01', '2019-03-17', 'ilovemedicine', null, 'Newbie', b'1', 'aspiring doctor'),
+          ('bambi', '2015-02-06', '2019-03-15', 'bambi123', 'male', 'Moderator', b'0', null),
+        ('Robert', '2016-02-01', '2019-03-01', 'k91ka', null, 'Senior Member',default,'Premed student at McGill Univserity'),
+        ('PlsAcceptMe', '2019-03-17', '2019-03-20', 'plsplspls', 'female', 'Newbie', b'0', null),
+        ('patj82','2018-12-04','2019-03-16','8j6753GHD',default,default,default,NULL),
+        ('Kex','2017-06-07','2019-03-17','jBB89kiokL','Female','Senior Member',default,NULL),			('lopo3','2012-11-03','2019-03-15','0987AbCd',default,'Moderator',default,'Our knowledge has made us cynical.
+Our cleverness, hard and unkind.
+We think too much and feel too little.
+More than machinery we need humanity.
+More than cleverness we need kindness and gentleness.
+Without these qualities, life will be violent and all will be lost.'), 
+		('jay8502','2017-02-02','2019-02-28','bnJD82Kj','Male','Senior Member',default,NULL),				('TopChef','2016-04-05','2019-03-17','bubuLULU99','Male','Senior Member',default,NULL),
+        ('Alex grey','2017-09-14','2019-03-11','phD66552ok','Female',default,default,NULL),						('zizoupandacad','2012-12-30','2019-03-16','panda6754Hy',default,'Newbie',default,NULL),					('montreal25','2015-10-21','2019-03-14','vbghJ9988',default,default,default,NULL),
+        ('eg0g','2017-10-03','2019-03-15','67ghjPlPbn','Male','Moderator',default,NULL),
+        ('Arztin90','2013-08-12','2019-01-07','JJJKKK9j9k','Female','Moderator',default,'test');
+        
+        
+# Create Forum relation 
+
+CREATE TABLE Forum (
+	forum_id SMALLINT AUTO_INCREMENT,
+	forum_title VARCHAR(100) NOT NULL,
+	PRIMARY KEY (forum_id) 
+); 
+
+# insert records into Forum relation
+
+INSERT INTO Forum (forum_title) VALUES 
+	('GENERAL PREMED AND MED SCHOOL TOPICS'), 
+    ('HEALTHCARE PROFESSIONS'), 
+    ('ONTARIO MEDICAL SCHOOLS'), 
+    ('QUEBEC MEDICAL SCHOOLS'), 
+    ('ATLANTIC MEDICAL SCHOOLS'), 
+    ('INFORMATION EXCHANGE'); 
+
+
+# Create Topic relation 
+
+CREATE TABLE Topic (
+	topic_id SMALLINT AUTO_INCREMENT,
+	topic_title VARCHAR(100) NOT NULL,
+	description text,
+	forum_id SMALLINT NOT NULL,
+	creator_username VARCHAR(20),
+	PRIMARY KEY (topic_id),
+	FOREIGN KEY (forum_id) REFERENCES Forum(forum_id),
+	FOREIGN KEY (creator_username) REFERENCES Member(username)
+);
+
+# insert records into Topic relation
+
+INSERT INTO Topic (topic_title, description, forum_id, creator_username) VALUES
+	('General Premed Discussions', 'Premed topics on Canadian med school admissions. Specific med school topics go below in their respective medical school forums.', 1, 'Robert'), 
+	('Medical Student General Discussions', 'An area for Canadian medical students to interact and share information.', 1, 'Robert'),
+	('Research Dicussions', 'Discuss research topics and opportunities here, including NSERC.', 1, 'bambi'), 
+	('Dental Student General Dicussions', 'An area for Canadian dental students to interact and share information.', 2, 'bambi'),
+	('Optometry Discussions', 'An area for Canadian optometry students and applicants to interact and share information.', 2, 'premed_67'), 
+	('Veterinary Medicine Dicussions', 'An area for Canadian veterinary students and applicants to interact and share information.', 2, 'premed_67'), 
+	('General Ontario Discussions (OMSAS)', 'General Ontario med school topics: eg. tuition, seats, cutoffs, OMSAS applications.', 3, 'Robert'),
+	('University of Western Ontario Medical School', null, 3, 'bambi'), 
+	('University of Toronto Medical School', null, 3, 'bambi'), 
+	('General Quebec Discussions', 'General Quebec premed and med school discussions, including CEGEP.', 4, 'premed_67'), 
+	('McGill Medical School', null, 4, 'Robert'),
+	('University of Sherbrooke Medical School', null, 4, 'premed_67'),
+	('Dalhousie University Medical School', null, 5, 'bazinga'),
+	('Memorial University Medical School', null, 5, 'bazinga'),
+	('Textbook and Equipement Reviews', 'Your Consumer Reports for medical textbooks and equipment.', 6, 'bazinga'), 
+	('Fore Sale/Trade Classifieds', 'Your place to sell old premed and medical items (eg. MCAT/DAT supplies, textbooks, etc). No dealers please.', 6, 'Robert'); 
+     
+     
+# Create Post relation
+
+CREATE TABLE Post ( 
+	post_id  SMALLINT NOT NULL AUTO_INCREMENT, 
+	post_title VARCHAR(200) NOT NULL,
+	content text NOT NULL,
+	post_date DATE,
+	post_time TIME,
+	topic_id SMALLINT NOT NULL,
+	written_by_username VARCHAR(20) NOT NULL,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (topic_id) REFERENCES Topic(topic_id),
+	FOREIGN KEY (written_by_username) REFERENCES Member(username)
+); 
+
+# insert records into Post relation
+INSERT INTO 
+Post(post_title,content,post_date,post_time,topic_id,written_by_username) VALUES ('UBC Invite/Regrets 2019', "Thought I'd start this thread. When do UBC invites come out?", '2019-01-07', '06:43:00', 4, 'Alex grey'); 
+
+INSERT INTO Post(post_title,content,post_date,post_time,topic_id,written_by_username) VALUES ('Dentist in the canadian forces/army', 'Hi everyone,
+
+ 
+
+I am starting my 3rd year of dental school in august and I applied to the canadian forces to become a military dentist. I wrote my aptitude test but they told me there were 7 people on the waiting list before me. I have friends in the army and the army recruiter that came last year told us they were almost "desperate" to recruit dental students so I was a bit surprised and disappointed that I would at least have to wait for another year (maybe two and by then I will have graduated...) to join the canadian forces.
+
+Is anyone in the same situation? Is anyone a dentist in the army at the moment? I would like to get your feedbacks about your experience! 
+
+ 
+
+Thank you,
+
+ 
+
+Doomz', '2018-07-22', '09:56:00', 4, 'lopo3');
+
+INSERT INTO Post(post_title,content,post_date,post_time,topic_id,written_by_username) VALUES ('FREE Study guides', 'I have FREE study guides for the MCAT, DAT, OAT, LSAT, PCAT, GRE, and GMAT. PM me if you want one! :)', '2018-11-24', '13:45:00', 16,'lopo3');
+
+INSERT INTO Post(post_title,content,post_date,post_time,topic_id,written_by_username) VALUES ('DATcrusher subscription available! For February 219 DAT practice','Hi ! 
+
+For anyone planning on writing the February 2019 DAT, my DATcrusher account is available until March 25, 2019. I am selling it for a discounted price since I am no longer using the account. 
+
+I wrote the Novemeber 2018 DAT and I highly recommend DATcrusher for Canadian students writing the DAT or need practice. 
+
+Please message me or reply to this for more details! :)','2018-11-15','12:44:00',16,'premed_67');
+
+
+INSERT INTO Post(post_title,content,post_date,post_time,topic_id,written_by_username) VALUES ('Thinking of applying to OMFS Programs in the USA?',"I'd like to be a resource for people. Something that I never had. 
+
+Feel free to ask any questions regarding the application process, CBSE, 4 vs 6, interviews, grades, etc.
+
+I'm a mid-level resident at a 6 year program. 
+
+NB",'2018-11-27','15:35:00',4,'lopo3');
+
+INSERT INTO Post(post_title,content,post_date,post_time,topic_id,written_by_username) VALUES ('UofT 2019 Application Discussion Thread','This is the place to speculate and ask questions.','2019-02-13','13:05:00',9,'Kex');
+
+
+INSERT INTO Post(post_title,content,post_date,post_time,topic_id,written_by_username) VALUES ('Writing MCAT in summer after 2nd year',"Hey,
+
+I'm in a bit of a dilemma. I want to write the MCAT this summer, but I'm not sure when I should take it. I wanna self-study it, and apparently 2 months are adequate if you don't have other commitments. However, I've looked at the test dates and almost all of the August ones are booked, so I don't know what to do. Write in July and try to re-write in August if I don't too well? Or do August test dates get rescheduled and will I have a chance to schedule for the date I'm interested in?",'2019-03-16','14:57:00',1,'Alex grey');
+
+
+INSERT INTO Post(post_title,content,post_date,post_time,topic_id,written_by_username) VALUES 
+('Research funding for medical students', "I am a first year medical student that is beginning to extensively involve myself in clinical research - I was wondering if there were any grants, scholarships, or bursaries that I could apply for to aid in this process. I am aware of the ones at my school, thanks!", '2018-03-23','15:05:00', 3, 'bazinga' ), 
+('How to research', "I'll be working on a research project this summer. It will be my first time analyzing data and writing a paper. Any tips on how to work on the project efficiently? Any guides on how to approach research?", '2018-01-31', '13:23:01', 3, 'Robert'), 
+('DMD mcgill', "as DMD results are approaching, (this upcoming wednesday), I wanted to know if there was a history of mcgill sending out results for DMD earlier than the target date, like monday or tuesday.", '2019-03-20', '06:03:00',11, 'PlsAcceptMe' ),
+('Post-interview impressions', "Now that the McGill interviews are all done, I am starting this thread to discuss how we feel about our interview experiences. (Of course, let’s not talk about specific questions or anything confidential).
+ 
+Personally, I feel great about three stations, awful about 2 stations that I’m pretty sure I’ve done poorly at, and ok about the rest.  The McGill MMI style is definitely unique. ", '2017-05-11', '18:30:01', 11, 'bazinga'); 
+
+
+# Create Reply relation
+
+CREATE TABLE Reply ( 
+	reply_id SMALLINT NOT NULL AUTO_INCREMENT,
+	content text NOT NULL, 
+	reply_date DATE,
+	reply_time TIME,
+	post_id SMALLINT NOT NULL, 
+ 	written_by_username VARCHAR(20) NOT NULL,
+	PRIMARY KEY (reply_id),
+	FOREIGN KEY (written_by_username) REFERENCES Member (username),
+	FOREIGN KEY (post_id) REFERENCES Post (post_id)
+); 
+
+# insert records into Reply relation
+
+INSERT INTO Reply (content, reply_date, reply_time, post_id, written_by_username)
+VALUES 
+	("Same
+
+I feel that i didn't stand out. I feel that i could done better. :( I really mess up 2-3 station as well.", '2017-05-11', '19:01:00', 8, 'Arztin90');
+
+INSERT INTO Reply (content, reply_date, reply_time, post_id, written_by_username)
+VALUES('Clinical research and case reports, thanks!','2018-05-23','11:20:00',6,'premed_67');
+
+INSERT INTO Reply (content, reply_date, reply_time, post_id, written_by_username)
+VALUES('what day was your mmi?','2019-02-23','09:45:00',8,'bambi');
+
+INSERT INTO Reply (content, reply_date, reply_time, post_id, written_by_username)
+VALUES('For MDCM too ? Or only DMD ?','2019-03-17','15:38:00',7,'bazinga');
+
+INSERT INTO Reply (content, reply_date, reply_time, post_id, written_by_username)
+VALUES('Strap yourself in, prepare for endless delays and frustration. Prepare to have your work scrutinized to no end. There is no such thing as efficiency. Its fun though. Enjoy, welcome to research.','2018-05-28','22:20:00',6,'doctorwho');
+
+
+
+# Create Reaction relation
+
+CREATE TABLE Reaction ( 
+	reaction_id INTEGER NOT NULL AUTO_INCREMENT,
+	type ENUM('like', 'sad', 'confused', 'haha', 'thanks') NOT NULL,
+	post_id SMALLINT,
+	reply_id SMALLINT,
+	from_username VARCHAR(20),
+	PRIMARY KEY (reaction_id),
+	FOREIGN KEY (post_id) REFERENCES Post(post_id),
+	FOREIGN KEY (reply_id) REFERENCES Reply(reply_id),
+	FOREIGN KEY (from_username) REFERENCES Member(username)
+); 
+
+INSERT INTO Reaction(type, post_id, reply_id, from_username) VALUES 
+	('like',8,null,'jay8502'), 
+    ('like',6,null,'patj82'),
+    ('sad',8,null,'Kex'),
+    ('confused',6,null,'doctorwho'),
+    ('haha',7,null,'bazinga'),
+    ('like',8, null, 'bambi'),
+    ('haha',8, null, 'premed_67'),
+    ('haha',8, null, 'Robert'),
+    ('like',8, null, 'doctorwho'),
+    ('haha',8, null, 'patj82'),
+    ('like',8, null, 'TopChef'),
+    ('thanks',8, null, 'zizoupandacad'),
+    ('sad', null, 4, 'doctorwho'),
+    ('sad', null, 4, 'premed_67'),
+    ('like', 5, null, 'Alex grey'); 
+   
+
+# Create Trophy relation
+
+CREATE TABLE Trophy ( 
+	place SMALLINT NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	date_won DATE NOT NULL,
+	PRIMARY KEY (place, date_won),
+	FOREIGN KEY (username) REFERENCES Member(username)
+); 
+
+# insert records into Trophy table
+
+INSERT INTO Trophy VALUES (1,'Robert','2018-09-21'),(2,'jay8502','2019-01-02'),(3,'Kex','2019-03-01'),(1,'Robert','2019-01-13'),(2,'Arztin90','2019-01-03');
+
+
+# Create Following relation
+
+CREATE TABLE Following(
+	username_following VARCHAR(20) NOT NULL,
+	username_being_followed VARCHAR(20) NOT NULL,
+	PRIMARY KEY (username_following, username_being_followed),
+	FOREIGN KEY (username_being_followed) REFERENCES Member(username),
+	FOREIGN KEY (username_following) REFERENCES Member(username)
+); 
+
+# insert records into Following relation
+
+INSERT INTO Following VALUES 
+	('Alex grey','Arztin90'),
+    ('future_doc','Arztin90'),
+    ('TopChef','bambi'),
+    ('Robert','bazinga'),
+    ('doctorwho','Robert'), 
+    ('bambi', 'TopChef'),
+    ('future_doc', 'bambi'); 
+    
+# Create Member_Follows_Topic relation
+
+CREATE TABLE Member_Follows_Topic ( 
+	topic_id SMALLINT NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	PRIMARY KEY (topic_id, username),
+	FOREIGN KEY (username) REFERENCES Member(username),
+	FOREIGN KEY (topic_id) REFERENCES Topic(topic_id)
+); 
+
+# insert records into Member_Follows_Topic relation 
+
+INSERT INTO Member_Follows_Topic VALUES 
+	(11,'Robert'),
+    (1,'Robert'),
+    (1,'Arztin90'),
+    (4,'patj82'),
+    (3,'bazinga'), 
+    (11, 'bambi'),
+    (11, 'future_doc'),
+    (11, 'premed_67'),
+    (11, 'bazinga');
+
+# Create Member_Manages_Forum relation
+
+CREATE TABLE Member_Manages_Forum (
+	forum_id SMALLINT NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	PRIMARY KEY (forum_id, username), 
+	FOREIGN KEY (forum_id) REFERENCES Forum(forum_id),
+	FOREIGN KEY (username) REFERENCES Member(username)
+);	
+
+# insert records into Member_Manages_Forum relation 
+
+INSERT INTO Member_Manages_Forum VALUES 
+	(4,'Robert'),
+    (1,'bazinga'),
+    (2,'bambi'),
+    (1,'Arztin90'),
+    (3,'eg0g');
+                 
---
+++ queries.sql
@@ -0,0 +1,226 @@
+
+/*
+Deliverable 3: INSY 437
+Names: Alexa Hernandez (260743067), Sasha Kahzdan (260666176), Vittoria Vecchioli (260720859), Yiyue Tan (260825127) 
+*/
+
+
+
+/*
+Query 1: List all members who are moderators and have used a reaction
+*/
+SELECT username
+FROM Member m
+WHERE m.member_rank='Moderator' AND m.username IN(SELECT from_username FROM Reaction)
+GROUP BY m.username;
+
+/*
+Query 2: List all members who manage a forum and have posted a reply
+*/
+SELECT username
+FROM Member m
+WHERE m.username IN(SELECT username FROM Member_Manages_Forum) AND m.username IN(SELECT written_by_username FROM Reply)
+GROUP BY username;
+
+/* 
+Query 3: A new post was created for the Topic with topic_title = "McGill Medical School". We need to generate a list of all usernames of members following the topic to send them a notification informing them of the new post.
+*/ 
+
+SELECT m.username
+FROM Topic t, Member_Follows_Topic m
+WHERE t.topic_id = m.topic_id AND
+	  t.topic_title = 'McGill Medical School'; 
+      
+      
+/* 
+Query 4: display the number of reactions, grouped by type of reaction, for the post with post_id = 8. If there are no reactions of a given type, the count should be 0. 
+*/ 
+
+SELECT t1.type, COALESCE(t2.num_reactions,0) as num_reactions
+FROM
+(SELECT DISTINCT type 
+FROM Reaction) t1 LEFT JOIN
+(SELECT type, COUNT(*) as num_reactions
+FROM Reaction
+WHERE post_id = 8 
+GROUP BY type) t2
+ON t1.type = t2.type
+;
+
+/* 
+Query 5: display the count of reactions, grouped by type of reaction, that member with username 'bazinga' has received on any of his/her post and/or replies. If 'bazinga' has never received a reaction of a certain type on any of his/her posts or replies, the count should be 0. 
+*/
+
+SELECT t1.type, COALESCE(t2.num_reactions,0) as num_reactions
+FROM
+(SELECT DISTINCT type 
+FROM Reaction) t1 LEFT JOIN
+(SELECT type, COUNT(*) as num_reactions
+FROM Reaction r 
+WHERE post_id IN (SELECT post_id FROM Post WHERE written_by_username = 'bazinga') OR
+	  reply_id IN (SELECT reply_id FROM Reply WHERE written_by_username = 'bazinga')
+GROUP BY type) t2
+ON t1.type = t2.type; 
+
+/* 
+Query 6: generate a list of the usernames of all members currently online 
+*/
+
+SELECT username 
+FROM Member
+WHERE active = 1; 
+
+/* 
+Query 7: display the username of the newest member along with the date joined.
+*/
+
+SELECT username, date_joined
+FROM Member
+WHERE date_joined >= ALL (SELECT date_joined FROM Member); 
+
+/* 
+Query 8: we want to generate popular contributor statistics! Namely, display the username of the member who generated the most content (the most posts and replies) in the year 2018, along with the content count. 
+*/
+
+
+SELECT p.written_by_username,COALESCE(p.num_posts,0) + COALESCE(r.num_replies,0) as content_count
+FROM
+(SELECT written_by_username, COUNT(*) AS num_posts
+ FROM Post 
+ WHERE post_date >= '2018-01-01' AND post_date <= '2018-12-31'
+GROUP BY written_by_username) p LEFT JOIN
+(SELECT written_by_username, COUNT(*) AS num_replies
+ FROM Reply 
+ WHERE reply_date >= '2018-01-01' AND reply_date <= '2018-12-31'
+GROUP BY written_by_username) r ON  p.written_by_username = r.written_by_username
+
+UNION
+
+SELECT r.written_by_username, COALESCE(p.num_posts,0) + COALESCE(r.num_replies,0) as content_count
+FROM
+(SELECT written_by_username, COUNT(*) AS num_posts
+ FROM Post 
+ WHERE post_date >= '2018-01-01' AND post_date <= '2018-12-31'
+GROUP BY written_by_username) p RIGHT JOIN
+(SELECT written_by_username, COUNT(*) AS num_replies
+ FROM Reply 
+ WHERE reply_date >= '2018-01-01' AND reply_date <= '2018-12-31'
+GROUP BY written_by_username) r ON  p.written_by_username = r.written_by_username
+ORDER BY content_count DESC
+LIMIT 1
+;
+
+/* 
+Query 9: Display the title and description of all topics, along with the number of post the topic currently contains. 
+If the topic description is null the string 'No description' should appear. 
+*/
+
+SELECT topic_title AS Title, COALESCE(description, 'No description') AS Description, COUNT(*) AS 'Number of Posts'
+FROM Topic t, Post p
+WHERE t.topic_id = p.topic_id
+GROUP BY topic_title, description;
+
+
+/*
+Query 10: Display all topics with post related to MCAT
+*/
+SELECT topic_title 
+FROM Topic t, Post p
+WHERE t.topic_id = p.topic_id and p.content LIKE "%MCAT%";
+
+/*
+Query 11: Display all post titles posted by a Senior Member that has received a trophy
+*/
+
+SELECT distinct(post_title) , m.username
+FROM Post p, Member m , Trophy t
+WHERE m.username = p.written_by_username 
+and m.username = t.username 
+and member_rank ="Senior Member" ;
+
+
+/*
+Query 12: Display all posts with replies written by newbie 
+*/
+
+select post_title, username
+from Post p, Reply r, Member m
+where p.post_id = r.post_id 
+and r.written_by_username = m.username
+and m.member_rank = "Newbie";
+
+/*
+Query 13: Display the reaction type the most used by members with member_rank = 'Newbie' 
+*/
+
+SELECT r.type, count(r.type) as count
+FROM Reaction r, Member m
+WHERE r.from_username = m.username AND m.member_rank = 'Newbie'
+GROUP BY r.type 
+HAVING count >= ALL (SELECT count(type) as count FROM Reaction r, Member m WHERE r.from_username = m.username AND member_rank = 'Newbie' GROUP BY type);
+
+
+/*
+Query 14: Display all members and the number of followers each member has 
+*/
+
+select username, count(username_following) as followers
+from Member m, Following f
+where m.username = f.username_being_followed
+group by username ;
+
+/*
+Query 15: Display the most popular topic (ie. the topic that is followed by most members) 
+*/
+
+select topic_title, count(f.topic_id) as followers
+from Topic t, Member_Follows_Topic f
+where t.topic_id = f.topic_id 
+group by topic_title
+order by count(f.topic_id) desc
+limit 1;
+
+/*
+Query 16: List members who are not moderators and have participated in discussions  
+*/
+
+select distinct m.username
+from Member m, Post p, Reply r
+where m.member_rank != 'Moderator' and (m.username=p.written_by_username or m.username=r.written_by_username);
+
+/*
+Query 17: List the number of posts that each topic contains 
+*/
+
+select t.topic_title, count(p.topic_id) as number_of_posts
+from Topic t, Post p
+where t.topic_id=p.topic_id
+group by t.topic_title;
+
+/*
+Query 18: Display the posts that contain at least two replies
+*/
+select p.post_title
+from Post p, Reply r
+where p.post_id=r.post_id 
+group by p.post_title
+having count(r.post_id)>=2;
+
+/*
+Query 19: Display the member who has received most trophies and the number of trophies he or she has received 
+*/
+
+select m.username, count(t.username)as number_of_trophy
+from Member m, Trophy t
+where m.username=t.username
+group by m.username
+order by count(t.username)desc
+limit 1;
+
+
+/*
+Query 20: List the username, last visited and rank for all the members that are being followed to by other members 
+*/
+select distinct f.username_being_followed,m.last_visited,m.member_rank
+from Member m, Following f
+where m.username=f.username_being_followed;
